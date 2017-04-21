f47171d10824ac2c18aa3ccc4655425d9d6dd88a
Rachel <rmcavin@gmail.com>
2017-04-20 23:15:02 -0500

    Finished basic calculator functionality

--- calc.js
+++ calc.js
@@ -1,1 +1,23 @@
 //Calculator structure
+
+const expressionStart = "";
+var expression = "";
+var answer = "";
+
+$(document).ready(function() {
+  $(".item").on("click", function() {
+    expression = expression.concat($(this).text());
+    console.log(expression);
+    $(".readOut").html(expression);
+  });
+  $(".clear").on("click", function() {
+    expression = expressionStart;
+    $(".readOut").html(expression);
+  });
+  $(".enter").on("click", function() {
+    answer = eval(expression);
+    $(".readOut").html(answer);
+    console.log(answer);
+    expression = answer;
+  });
+});
